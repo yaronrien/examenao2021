c28a8f7217cee46b822ec797b104696105b264ba
Yaron Riensema <97059603@st.deltion.nl>
2021-05-31 11:54:48 +0200

    navbar change

--- assets/php/partials/navbar.php
+++ assets/php/partials/navbar.php
@@ -2,9 +2,6 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #6EDFF6">
   <div class="container-fluid">
     <a class="navbar-brand" style="color: #fff" href="#">DVG</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
     <ul class="nav navbar-nav navbar-right">
       <li><a class="btn btn-success" href="./leden/index.php"> Leden</a></li>
     </ul>
