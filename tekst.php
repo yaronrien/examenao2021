e6d472543eac12628e7e0212307d9543910b06a7
Ihan <i.sadiq@mmc-itsolutions.nl>
2021-05-31 11:16:46 +0200

    Added tekst.php

---
+++ tekst.php
@@ -0,0 +1,28 @@
+<?php
+    include_once 'assets/php/partials/navbar.php';
+?>
+
+<!DOCTYPE HTML>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
+</head>
+<body>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="form-group">
+      
+  <div class="form-group">
+  <h1 class="h1 text-center mb-3">Tekst schrijven</h1>
+  <h4 class="h4 text-center mb-3">max 500 karakters</h4>
+    <input class="form-control input-sm" id="inputsm" type="text">
+  </div>
+        <textarea class="form-control rounded-0" id="exampleFormControlTextarea1" rows="10"></textarea>
+        <label> Aantal Woorden: X </label>
+        <label> Aantal Letters: X </label>
+      </div>
+    </div>
+</body>
+</html>
