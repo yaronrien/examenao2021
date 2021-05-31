d2cebb352852b42d7d95c3be5e1c85b7dbdad839
jankeeske <85103567+jankeeske@users.noreply.github.com>
2021-05-31 11:58:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <div class="text-center">
             <h1>Wie zijn wij?</h1>
             <p>De Geletterde Vrienden(DGV) is een stichting die zich bezighoudt met de Nederlandse taal.</p>
-            <a class="btn btn-lg btn-secondary" href="mailto:info@degeletterdevrinden?subject=Account%20Aanvraging&body=Goedendag%2C%0A%0AIk%20zou%20mij%20graag%20aanwillen%20melden%20voor%20jullie%20site!%0A%0AMet%20vriendelijke%20groet%2C%0A" role="button">Meld je aan!</a>
+            <a class="btn btn-lg btn-secondary" href="mailto:info@degeletterdevrinden?subject=Account%20Aanvraaging&body=Goedendag%2C%0A%0AIk%20zou%20mij%20graag%20aanwillen%20melden%20voor%20jullie%20site!%0A%0AMet%20vriendelijke%20groet%2C%0A" role="button">Meld je aan!</a>
         </div>
     </div>
 </body>
