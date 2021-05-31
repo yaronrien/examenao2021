f6a7c38ba90fe86c2a20a86568c29bee51f2b6f2
Yaron Riensema <97059603@st.deltion.nl>
2021-05-31 11:09:03 +0200

    home

--- assets/php/partials/navbar.php
+++ assets/php/partials/navbar.php
@@ -1,12 +1,12 @@
 <!--Bootstrap Menu-->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #6EDFF6">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">BVG</a>
+    <h1 class="display-1"><a class="navbar-brand" href="#">DVG</a></h1>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"> Leden</a></li>
+      <li><a class="btn btn-lg btn-success" href="#" role="button"> Leden</a></li>
     </ul>
   </div>
 </nav>
--- index.php
+++ index.php
@@ -9,3 +9,12 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
+<body>
+    <div class=" mt-5 container">
+        <div class="text-center">
+            <h1>Wie zijn wij?</h1>
+            <p>De Geletterde Vrienden(DGV) is een stichting die zich bezighoudt met de Nederlandse taal.</p>
+            <a class="btn btn-lg btn-secondary" href="#" role="button"> Meld je aan!</a>
+        </div>
+    </div>
+</body>
