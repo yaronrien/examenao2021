fafca2442c665ebebe4f453afef77cbb5816d673
MRietdijk <mart.rietdijk@gmail.com>
2021-05-31 10:42:34 +0200

    added html tags

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
 <!DOCTYPE HTML>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
 </head>
+<body>
+    
+</body>
+</html>
