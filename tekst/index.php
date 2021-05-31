44e9a12dfead5a28f081fde4d5e1a499d2a84c7d
Ihan <i.sadiq@mmc-itsolutions.nl>
2021-05-31 14:35:15 +0200

    Added PDO query and changed url in navbar

--- tekst/index.php
+++ tekst/index.php
@@ -1,6 +1,24 @@
 <?php
     include_once '../assets/php/partials/navbar.php';
-?>
+    include_once '../assets/php/partials/config.php';
+ 
+    if(isset($_POST['submit'])){
+       try{
+            $connection = new PDO($dsn, $username, $password, $options);
+        }catch(PDOException $error){
+            echo 'Oh jeetje.. Check je database gegevens.. ';
+        } 
+
+        $stmt = $connection->prepare("INSERT INTO teksten (tekstTitel, tekst, toevoegDatum) VALUES(:tekstTitel, :tekst, NOW())");
+
+        $stmt -> bindParam(":tekstTitel", $_POST["tekstTitel"], PDO::PARAM_STR);
+        $stmt -> bindParam(":tekst", $_POST["tekst"], PDO::PARAM_STR);
+
+        $stmt->execute();
+
+    }
+ 
+  ?>
 
 <!DOCTYPE HTML>
 <html>
@@ -9,7 +27,7 @@
     <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.css">
 </head>
 <body>
-
+<form method="post">
 <div class="container">
   <div class="row">
     <div class="col-md-12">
@@ -18,9 +36,11 @@
   <div class="form-group">
   <h1 class="h1 text-center mb-3">Tekst schrijven</h1>
   <h4 class="h4 text-center mb-3">max 500 karakters</h4>
-    <input class="form-control input-sm" id="inputsm" type="text">
+    <input class="form-control input-sm" name="tekstTitel" maxlength = "80"  type="text" placeholder="Titel">
     </div>
-        <textarea class="form-control rounded-0" id="exampleFormControlTextarea1" rows="10" onkeyup="countCharacters(this.value)"></textarea>
+        <textarea class="form-control rounded-0" name="tekst" maxlength = "500" rows="10" onkeyup="countCharacters(this.value)"
+        placeholder="Schrijf een mooie tekst hier!, misschien over jankees? let wel wel dat je maximaal 500 tekens mag schrijven!" >
+        </textarea>
         <label> Aantal Letters: <span id="characterCount">0</span>/500 </label>
     </div>
   </div>
@@ -28,7 +48,9 @@
     let characterSpan = document.getElementById('characterCount')
     function countCharacters(text) {
       characterSpan.innerText = text.length
-    }
+      }
+      
   </script>
+  <input type="submit" name="submit" value="Tekst versturen">
 </body>
 </html>
