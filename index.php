946dce18a42a5226ec577339bd7a38bead96b373
Yaron Riensema <97059603@st.deltion.nl>
2021-05-31 10:18:10 +0200

    navbar

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    include_once 'assets/php/partials/navbar.php';
+?>
+
+<!DOCTYPE HTML>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
