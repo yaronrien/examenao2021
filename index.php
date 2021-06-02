33745334575175d9ecceb4a2e957547f06dc2f3f
yaronrien <85099345+yaronrien@users.noreply.github.com>
2021-06-02 13:50:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
             <p>De Geletterde Vrienden(DGV) is een stichting die zich bezighoudt met de Nederlandse taal.</p>
             <p>Stichting De Geletterde Vrinden (DGV) is een stichting die zich interesseert in tekststatistieken.</p> 
             <p>In de huidige tijd worden veel woorden door emojicons vervangen.</p>
-            <p>Dat doet de ons pijn in onze geletterde hart en wij willen dat de mensen weer meer letters dan emojicons gaan gebruiken.</p>
+            <p>Dat doet ons pijn in onze geletterde hart en wij willen dat de mensen weer meer letters dan emojicons gaan gebruiken.</p>
             <p>Op onze nieuwe website kunt u zich aanmelden en zelf een tekst versturen en daarna kunt u alle statistieken zien</p>
             <p>Alle statistieken van de ingezonden woorden zijn ook beschikbaar, meem een kijkje!</p>
             <p>Schrijf daarom nu in via deze knop hieronder.</p>
